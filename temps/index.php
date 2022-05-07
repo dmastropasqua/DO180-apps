dfb44b025b90b2c5940e4af45b3ff20a79d358eb
Student User <student@workstation.lab.example.com>
2022-05-07 03:28:04 -0400

    update da console

--- temps/index.php
+++ temps/index.php
@@ -8,7 +8,7 @@
 
 <table>
 <tr>
-    <td>PLEASE Enter the temperature to convert:</td>
+    <td>PREGO INSERIRE LA TEMPERATURA DA CONVERTIRE:</td>
     <td><input type="text" name="temp" id="temp" size="10"></td>
 </tr>
 
