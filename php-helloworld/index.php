468d93c19eb96289d1beacebc26bb5d354f3a86a
Student User <student@workstation.lab.example.com>
2022-05-07 02:53:54 -0400

    yet another update

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,7 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is in the air!\n";
+print "<html>\n";
+print "Hello, World! php version is " . PHP_VERSION . "<br>\n";
+print "A change is in the air!<br>\n";
+print "THIS IS ANOTHER CHANGE! <br>\n";
+print "</html>\n";
 ?>
